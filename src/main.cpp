58b43ee7a2ac9749e00701f6fe49e896723d926e
carm <carm@carm.cc>
2025-03-31 17:10:25 +0800

    style: 整理代码变量结构

--- src/main.cpp
+++ src/main.cpp
@@ -10,52 +10,52 @@
 #define MAX_TIMESTAMPS 100   // 最大时间戳数量，同时代表取样时长内最大可记录液滴数
 #define DISPLAY_REFRESH_INTERVAL 500 // 显示屏刷新间隔，单位：ms
 
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, 13, 12, 11, 15);  //屏幕初始化
-DropletCounter V1(MAX_TIMESTAMPS, SAMPLING_DURATION, TRIGGER_THRESHOLD, A14);
-DropletCounter V2(MAX_TIMESTAMPS, SAMPLING_DURATION, TRIGGER_THRESHOLD, A13);
-DropletCounter V3(MAX_TIMESTAMPS, SAMPLING_DURATION, TRIGGER_THRESHOLD, A12);
+unsigned long current_millis;
+U8G2_ST7920_128X64_F_SW_SPI lcd(U8G2_R0, 13, 12, 11, 15);  //屏幕初始化
+DropletCounter sensor1(MAX_TIMESTAMPS, SAMPLING_DURATION, TRIGGER_THRESHOLD, A14);
+DropletCounter sensor2(MAX_TIMESTAMPS, SAMPLING_DURATION, TRIGGER_THRESHOLD, A13);
+DropletCounter sensor3(MAX_TIMESTAMPS, SAMPLING_DURATION, TRIGGER_THRESHOLD, A12);
 
 /**
  * 显示数据到屏幕
- * @param droplet 液滴结构体指针
- * @param str     显示前缀
- * @param offset  显示行偏移
+ * @param droplet  液滴结构体指针
+ * @param str      显示前缀
+ * @param yOffset  显示行像素偏移量
  */
-void draw_data(DropletCounter *droplet, const char str[], int offset) {
-    u8g2.setFont(u8g2_font_ncenB10_tr);
-    u8g2.setCursor(0, offset);
-    u8g2.print(str);
-    u8g2.setCursor(15, offset);
-    u8g2.print(droplet->getDropletsPerSecond(), 1); // 保留1位小数
-    u8g2.setCursor(55, offset);
-    u8g2.print("D/s");
+void draw_data_line(DropletCounter *droplet, const char str[], const int yOffset) {
+    lcd.setCursor(0, yOffset);
+    lcd.print(str);
+    lcd.setCursor(15, yOffset);
+    lcd.print(droplet->getDropletsPerSecond(), 1); // 保留1位小数
+    lcd.setCursor(55, yOffset);
+    lcd.print("D/s");
 }
 
 void update_display() {
-    unsigned long current_millis = millis();
-    V1.calculate(current_millis);
-    V2.calculate(current_millis);
-    V3.calculate(current_millis);
+    sensor1.calculate(current_millis);
+    sensor2.calculate(current_millis);
+    sensor3.calculate(current_millis);
 
-    u8g2.firstPage();
+    lcd.firstPage();
     do {
-        draw_data(&V1, "1:", 15);
-        draw_data(&V2, "2:", 35);
-        draw_data(&V3, "3:", 55);
-    } while (u8g2.nextPage());
+        lcd.setFont(u8g2_font_ncenB10_tr);
+        draw_data_line(&sensor1, "1:", 15);
+        draw_data_line(&sensor2, "2:", 35);
+        draw_data_line(&sensor3, "3:", 55);
+    } while (lcd.nextPage());
 }
 
 void setup() {
     Serial.begin(9600);
     MsTimer2::set(DISPLAY_REFRESH_INTERVAL, update_display);
     MsTimer2::start();
-    u8g2.begin();
+    lcd.begin();
     pinMode(14, OUTPUT);
 }
 
 void loop() {
-    unsigned long current_millis = millis();
-    V1.update(current_millis);
-    V2.update(current_millis);
-    V3.update(current_millis);
+    current_millis = millis();
+    sensor1.update(current_millis);
+    sensor2.update(current_millis);
+    sensor3.update(current_millis);
 }
